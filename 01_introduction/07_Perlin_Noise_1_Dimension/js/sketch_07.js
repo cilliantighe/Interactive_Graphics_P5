43e636956d3cf039b31d7faccc6811ca8500b5f9
Cillian Tighe <N00152737@iadt.ie>
2018-01-24 20:36:09 +0000

    Completed sketch 07

---
+++ 01_introduction/07_Perlin_Noise_1_Dimension/js/sketch_07.js
@@ -0,0 +1,61 @@
+/*
+Interactive Graphics
+Author: Cillian Tighe
+Student No: N00152737
+Sketch_07
+*/
+
+// Variable to act as an object of the 'Walker' class
+var walker;
+
+// The 'setup' function is only called once. Everything within the function is executed once
+function setup() {
+
+  // Creates the canvas for the animation to be displayed on
+  // Gets the width of the div so the canvas can take up all the space
+  var cWidth = $("#canvas").width();
+  var cHeight = 500;
+  var canvas = createCanvas(cWidth, cHeight);
+
+  // Repositioning the canvas
+  canvas.parent('canvas');
+
+  // Setting the color of the canvas background
+  background(255);
+
+  // Creating an object of the 'walker' class
+  walker = new Walker();
+}
+
+// The 'draw' function is called in a loop. Everything that is in the function is executed continuously
+function draw() {
+
+  noStroke();
+  fill(255, 50);
+  rect(0, 0, width, height)
+  // Calling the render and step function in a loop
+  walker.render();
+  walker.step();
+}
+
+function Walker() {
+  // Positioning the 'walker' in the centre of the canvas
+  this.x = width / 2;
+  this.xOff = random(width);
+
+  // Creating a function called 'render' that draws a point at the given co-ordinates
+  this.render = function() {
+    noStroke();
+    fill(255, 0, 200, 50);
+    ellipse(this.x, height/2, 40, 40);
+  }
+
+  // Creating a function called 'step' to move the current position of the 'ellipse'
+  this.step = function() {
+
+
+    //Constraining the point within the canvas
+    this.x = map(noise(this.xOff), 0, 1, 0, width);
+    this.xOff += 0.005;
+  }
+}
